2b14993dccf389528702e1e963774780efa791d6
krishnasanjay27 <Krishnasanjaybangaru@gmail.com>
2025-07-05 16:14:58 +0530

    crud postendpoint fixed

--- src/Js/SubTable/Crud/FormLoad/DOMContentLoaded/BuildBSTables/HtmlTable/OnPostBody/TableFooterSaveButtonId/FetchFromFuncs/PostFetch.js
+++ src/Js/SubTable/Crud/FormLoad/DOMContentLoaded/BuildBSTables/HtmlTable/OnPostBody/TableFooterSaveButtonId/FetchFromFuncs/PostFetch.js
@@ -1,9 +1,11 @@
 import { StartFunc as StartFuncFetchHeaders } from "./FetchHeaders/EntryFile.js";
-import ConfigJson from "../../../../../../../Config.json" with { type: "json" };
+import UrlJson from "./url.json" with { type: "json" };
+import ConfigJson from "../../../../../../../../Config.json" with { type: "json" };
 let StartFunc = async () => {
-    let jVarLocalPostUrl = ConfigJson.PostEndPoint;
+    let jVarTableName = ConfigJson.TableName;
+    let LocalroutePath = UrlJson.PostendPoint;
     let jVarLocalFetchHeaders = StartFuncFetchHeaders();
-    let jVarLocalFetchUrl = `${jVarLocalPostUrl}`;
+    let jVarLocalFetchUrl = `${jVarTableName }/${LocalroutePath}`;
     let response = await fetch(jVarLocalFetchUrl, jVarLocalFetchHeaders);
 
     return await response;
