6e0867ceb6697f29d1640dca3d7aec1a7c4fffbe
krishnasanjay27 <Krishnasanjaybangaru@gmail.com>
2025-07-07 11:42:22 +0530

    changed endpoints

--- src/Js/SubTable/subTableInsert/FormLoad/DOMContentLoaded/BuildBsTable/OnLoadSuccess/TableFooterSaveButtonId/FetchFromFuncs/PostFetch.js
+++ src/Js/SubTable/subTableInsert/FormLoad/DOMContentLoaded/BuildBsTable/OnLoadSuccess/TableFooterSaveButtonId/FetchFromFuncs/PostFetch.js
@@ -1,14 +1,16 @@
 import { StartFunc as StartFuncFetchHeaders } from "./FetchHeaders/EntryFile.js";
 import ConfigJson from "../../../../../../config.json" with { type: "json" };
+import CommonConfigJson from "../../../../../../../Config.json" with { type: "json" };
 
 let StartFunc = async () => {
     let jVarLocalPostUrl = ConfigJson.PostendPoint;
+    let jVarLocalTableName = CommonConfigJson.TableName;
 
     let jVarLocalFilterString = getUrlQueryParams({ inGetKey: "pk" });
 
     let jVarLocalFetchHeaders = StartFuncFetchHeaders();
 
-    let jVarLocalFetchUrl = `${jVarLocalPostUrl}/${jVarLocalFilterString}/ITEMS`;
+    let jVarLocalFetchUrl = `${jVarLocalTableName}/${jVarLocalPostUrl}/${jVarLocalFilterString}/ITEMS`;
     
     let response = await fetch(jVarLocalFetchUrl, jVarLocalFetchHeaders);
 
--- src/Js/SubTable/subTableShow/FormLoad/AddListeners/RefreshBSTableClass/FetchAsGet/fetchFuncs.js
+++ src/Js/SubTable/subTableShow/FormLoad/AddListeners/RefreshBSTableClass/FetchAsGet/fetchFuncs.js
@@ -1,9 +1,14 @@
 import getUrlJson from './getUrl.json' with {type: 'json'};
+import ConfigJson from '../../../../../Config.json' with {type: 'json'};
 
 let StartFunc = async () => {
     let jVarLocalFetchUrl = getUrlJson.GetEndPoint;
+    
+    let jVarLocalTableName = ConfigJson.TableName;
 
-    let response = await fetch(jVarLocalFetchUrl);
+    let jVarFetchUrl = `${jVarLocalTableName}/${jVarLocalFetchUrl}`;
+
+    let response = await fetch(jVarFetchUrl);
 
     return await response;
 };
